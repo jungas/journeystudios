6be163d97df3eb3b0de06f480230b0155e0b411d
Kyeongho Shin <2168473@slu.edu.ph>
2017-12-26 20:05:21 +0800

    added an image

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <html>
    <head>
-      <title>Journey Studios</title>      
+      <title>Journey Studios</title>
    </head>
-   
+
    <body>
-      
+      <img src="2.png"/>
       <?php
          echo "<h1>Hello, Journey Studios!</h1>";
       ?>
-   
+
    </body>
 </html>
